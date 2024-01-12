678499b8b1b6091af0867b361abc6bd1dd0f2bc6
Björn Ritzl <bjorn.ritzl@gmail.com>
2024-01-12 11:08:32 +0100

    Incorrect formatting of the config file

--- prometheus.lua
+++ prometheus.lua
@@ -1,25 +1,25 @@
 return {
     -- The default LuaVersion is Lua51
-    LuaVersion = "Lua51"; -- or "LuaU"
+    LuaVersion = "Lua51", -- or "LuaU"
     -- All Variables will start with this prefix
-    VarNamePrefix = "";
+    VarNamePrefix = "",
     -- Name Generator for Variables that look like this: b, a, c, D, t, G
-    NameGenerator = "MangledShuffled";
+    NameGenerator = "MangledShuffled",
     -- No pretty printing
-    PrettyPrint = false;
+    PrettyPrint = false,
     -- Seed is generated based on current time 
     -- When specifying a seed that is not 0, you will get the same output every time
-    Seed = 0;
+    Seed = 0,
     -- Obfuscation steps
     Steps = {
         {
             -- This obfuscation step puts all constants into an array at the beginning of the code
-            Name = "ConstantArray";
+            Name = "ConstantArray",
             Settings = {
                 -- Apply to Strings only
-                StringsOnly = true;
+                StringsOnly = true,
                 -- Apply to all Constants, 0.5 would only affect 50% of strings
-                Treshold    = 1;
+                Treshold    = 1,
             },
         },
     }
